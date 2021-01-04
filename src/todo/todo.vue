c35da4725ab516d433ac661d9b342b7b4953d215
Haoroyi <haoroyi@yeah.net>
2021-01-04 23:20:06 +0800

    业务逻辑

--- src/todo/todo.vue
+++ src/todo/todo.vue
@@ -1,58 +1,98 @@
 <template>
-    <section class="real-app">
-        <input 
-            type="text" 
-            class="add-input" 
-            autofocus="autofocus"
-            placeholder="接下去要做什么？"
-            @keyup.enter="addTodo"
-        >
-        <Item :todo="todo" />
-    </section>
-    
+  <section class="real-app">
+    <input
+      type="text"
+      class="add-input"
+      autofocus="autofocus"
+      placeholder="接下去要做什么？"
+      @keyup.enter="addTodo"
+    />
+    <Item
+      :todo="todo"
+      v-for="todo in filteredTodos"
+      :key="todo.id"
+      @del="deleteTodo"
+    />
+    <Tabs
+      :filter="filter"
+      :todos="todos"
+      @toggle="handleStateChange"
+      @clear="clearTodos"
+    />
+  </section>
 </template>
 <script>
-import Item from "./item.vue"
+import Item from "./item.vue";
+import Tabs from "./tabs.vue";
+let id = 0;
 export default {
-    components: {
-        Item
+  components: {
+    Item,
+    Tabs,
+  },
+  data() {
+    return {
+      todos: [],
+      filter: "all",
+    };
+  },
+  methods: {
+    addTodo(e) {
+      this.todos.unshift({
+        id: id++,
+        content: e.target.value.trim(),
+        completed: false,
+      });
+      e.target.value = "";
     },
-    data() {
-        return {
-            todo: {
-                id: 0,
-                content: 'This is a todo.',
-                completed: false
-            }
-        }
+    deleteTodo(id) {
+      this.todos.splice(
+        this.todos.find((item) => item.id === id),
+        1
+      );
     },
-    methods: {
-        addTodo() {}
-    }
-}
+    handleStateChange(state) {
+      this.filter = state;
+    },
+    clearTodos() {
+      this.todos = this.todos.filter( item => !item.completed);
+    },
+  },
+  computed: {
+    filteredTodos() {
+      if (this.filter === "all") {
+        return this.todos;
+      }
+      const completed = this.filter === "completed";
+      return this.todos.filter((item) => completed === item.completed);
+    },
+  },
+};
 </script>
 <style lang="stylus" scoped>
-.real-app
-    width 600px
-    margin 0 auto
-    box-shadow 0 0 5px #666666
-    .add-input
-        position relative
-        width 100%
-        font-size 24px
-        font-family inherit
-        font-weight inherit
-        line-height 1.4em
-        border 0
-        outline none
-        color inherit
-        padding 6px
-        border 1px solid #999
-        box-shadow inset 0 -1px 5px 0 rgba(0,0,0,.5)
-        box-sizing border-box
-        font-smoothing antialiased
-        padding 16px 16px 16px 60px
-        border none
-        box-shadow inset 0 -2px 1px rgba(0,0,0,.5)
+.real-app {
+  width: 600px;
+  margin: 0 auto;
+  box-shadow: 0 0 5px #666666;
 
+  .add-input {
+    position: relative;
+    width: 100%;
+    font-size: 24px;
+    font-family: inherit;
+    font-weight: inherit;
+    line-height: 1.4em;
+    border: 0;
+    outline: none;
+    color: inherit;
+    padding: 6px;
+    border: 1px solid #999;
+    box-shadow: inset 0 -1px 5px 0 rgba(0, 0, 0, 0.5);
+    box-sizing: border-box;
+    font-smoothing: antialiased;
+    padding: 16px 16px 16px 60px;
+    border: none;
+    box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.5);
+  }
+}
 </style>
